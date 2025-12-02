44354b2eadaf7215602dec355c8eb2dc98c9df06
Aman Jain <aman0012001@gmail.com>
2025-12-03 00:57:26 +0530

    Firebase: apply google-services plugin in app and remove committed google-services.json

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0"
     id("org.jetbrains.kotlin.plugin.compose")
+    id("com.google.gms.google-services")
 }
 
 android {
