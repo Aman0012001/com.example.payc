ce3dcb15acf067df5eb0246fcad010d0a74d1c9d
Aman Jain <aman0012001@gmail.com>
2025-12-03 10:27:51 +0530

    CI: gate build/upload on secrets; align Kotlin 2.0 + AGP 8.5.1; apply google-services in app

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "8.1.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.android.application") version "8.5.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
 
     // Firebase Google Services plugin
     id("com.google.gms.google-services") version "4.4.4" apply false
